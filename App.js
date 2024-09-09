2f9ddeb7200653706b6c04865783931b756f0a54
divya18k <151612109+divya18k@users.noreply.github.com>
2024-09-10 00:16:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import './stylesheets/alignment.css';
+import './stylesheets/theme.css';
+import './stylesheets/sizes.css';
+import './stylesheets/custom-components.css';
+import './stylesheets/form-elements.css';
+
+
+function App() {
+  return (
+    <div className="bg-primary flex justify-center items-center h-screen">
+      <h1 className='text-secondary'>Library management App</h1>
+      
+    </div>
+  );
+}
+
+export default App;
